95c89bbaa9c14d1635cf2d0affccddbe0631f1f8
Ofer <ofergavri@gmail.com>
2024-05-20 23:06:38 +0300

    update migrations

--- knexfile.ts
+++ knexfile.ts
@@ -4,7 +4,7 @@ import type { Knex } from 'knex';
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: process.env.POSTGRES_PRISMA_URL,
+  connection: process.env.POSTGRES_PRISMA_URL + (process.env.NODE_ENV === 'development' ? '' : 'sslmode=require'),
   migrations: {
     extension: 'ts',
     tableName: 'knex_migrations',
