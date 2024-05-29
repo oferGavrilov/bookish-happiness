4999dcc27a33b926255f3b1662b4be763dc92c07
Ofer <ofergavri@gmail.com>
2024-05-29 22:58:06 +0300

    added delete and update availability product

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -3,7 +3,7 @@
 import { z } from "zod"
 import db from '@/db/db';
 import fs from 'fs/promises'
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 const fileSchema = z.instanceof(File, { message: "Required" })
 const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
@@ -45,4 +45,19 @@ export async function addProduct(prevState: unknown, formData: FormData) {
     })
 
     redirect("/admin/products")
+}
+
+export async function toggleProductAvailability(id: string, isAvailableForPurchase: boolean) {
+    await db.product.update({
+        where: { id },
+        data: { isAvailableForPurchase }
+    })
+}
+
+export async function deleteProduct(id: string) {
+    const product = await db.product.delete({ where: { id } })
+    if (product == null) return notFound()
+
+    await fs.unlink(product.filePath)
+    await fs.unlink(`public${product.imagePath}`)
 }
